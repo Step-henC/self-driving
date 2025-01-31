4a5c8e7d7b68a3fb7188c965948a256998756248
Step-henC <stevecunningham@outlook.com>
2025-01-30 21:03:23 -0600

    left and right working well

--- car.js
+++ car.js
@@ -9,19 +9,23 @@ class Car {
         this.acceleration=0.2;
         this.maxSpeed =3;
         this.friction=0.05;
-        this.angle=0;
+        this.angle=0; //car can exceed maxSpeed at left, right movements, need this to save physics
         this.controls = new Controls()
     }
 
     draw(ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y); //translate based on where we want ctx centered at
+        ctx.rotate(-this.angle);
         ctx.beginPath();
         ctx.rect(
-            this.x - this.width/2, //x is gonna be center
-            this.y - this.height/2,
+            -this.width/2, //x is gonna be center
+            -this.height/2,
             this.width,
             this.height
         )
         ctx.fill();
+        ctx.restore(); //must restore otherwise translate and rotate infinitely
     }
 
     update(){
@@ -52,12 +56,22 @@ class Car {
 
         }
 
-        if (this.controls.left) {
-            this.angle -= 2;
+        if (this.speed !=0) { //speed polarity is direction
+            const flip = this.speed>0?1:-1; // if flipping direction (going backwards) then flip controlsfy
+            if (this.controls.left) {
+                this.angle += 0.03*flip; //this way left is always left
+            }
+            if (this.controls.right){
+                this.angle -=0.03*flip; //as oppose to left turning right, when going backwards
+            }
         }
-        if (this.controls.right){
-            this.x +=2
-        }
-        this.y -= this.speed;
+
+        
+
+        //car will rotate, but not move in direction of angle
+        //but rather a linear up and down, regardless of car position
+        //using the unit circle, radius of 1, sin is between -1 and 1
+        this.x -= Math.sin(this.angle)*this.speed;
+        this.y -= Math.cos(this.angle)*this.speed;
     }
 }
